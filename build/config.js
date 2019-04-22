89a63c1591b523f89f46564f70699e58b9e24d30
chenqf <qifeng.chen1006@foxmail.com>
2019-04-22 14:11:07 +0800

    typeof

--- build/config.js
+++ build/config.js
@@ -1,13 +1,18 @@
 
 module.exports = {
     wait:[
+        '请指出document.onload和document.ready两个事件的区别',
+        'event loop',
+        '变量提升，函数声明变函数表达式',
+        'try catch ',
+
         '深度好文，https://juejin.im/post/5cb0315f518825215e61ec14?utm_source=gold_browser_extension',
         'await 在forEach 中 https://juejin.im/post/5cb1d5a3f265da03587bed99?utm_source=gold_browser_extension',
         'typeof instanceOf isPrototypeOf() 之间的区别  https://juejin.im/post/5c9216716fb9a070ef60a011?utm_source=gold_browser_extension',
-        '请指出document.onload和document.ready两个事件的区别',
+
         '手写代码： https://juejin.im/post/5c9c3989e51d454e3a3902b6?utm_source=gold_browser_extension#heading-19',
-        'event loop',
-        '变量提升，函数声明变函数表达式',
+
+
         '重排与重绘 https://mp.weixin.qq.com/s?__biz=MzA5NzkwNDk3MQ==&mid=2650588825&idx=1&sn=9ce03764375441ef0a8eb4fbce702e7a&chksm=8891d6bdbfe65fab0e76b1f86ec807ab0d13dec92951604c59fd1fe23ab0619257bf1dfa32bf&scene=0&xtrack=1#rd',
         '防抖和节流',
         'css 剪切',
@@ -18,9 +23,7 @@ module.exports = {
         '1000-div问题 https://github.com/airuikun/Weekly-FE-Interview/issues/15',
         '开放题：2万小球问题 https://github.com/airuikun/Weekly-FE-Interview/issues/16',
         'http 缓存',
-        'bind 的模拟实现',
         'http 与 https 对称加密，非对称加密',
-        '手写代码实现 call apply bind https://juejin.im/post/5ca9de22e51d452b5372ed90?utm_source=gold_browser_extension',
         'promise 原生实现 https://github.com/YvetteLau/Blog/issues/2',
         '真机调试：https://juejin.im/post/5c947f5251882568396a6773?utm_source=gold_browser_extension',
     ],
@@ -31,6 +34,10 @@ module.exports = {
             name:'javascript 基础',//分类的标题
             path:'javascript基础',//分类的路径
             children:[
+                {
+                    name:'详解typeof操作符',
+                    issues:''
+                },
                 {
                     name:'如何中断forEach循环',
                     issues:''
